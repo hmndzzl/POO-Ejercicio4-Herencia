ea628173ea311685617ea3d7862dd1eba90cb62f
Diego-glitch-cloud <dcalderon918@gmail.com>
2024-10-01 21:44:09 -0600

    Atributo Longitud Reptir Acuatico & parte método 1 reptil

--- Reptil.java
+++ Reptil.java
@@ -1,22 +1,23 @@
 public class Reptil extends Animal {
-    protected double temperaturaCorporal;
-    protected int cantidadHuevos;
-    protected double peso;
-    protected boolean enPeligroExtincion;
-    protected String dieta;
+	protected double temperaturaCorporal;
+	protected int cantidadHuevos;
+	protected double peso;
+	protected boolean enPeligroExtincion;
+	protected String dieta;
 
-    //Constructor
-    public Reptil(String nombreCientifico, String descripcionHabitat, int esperanzaVida, double temperaturaCorporal, int cantidadHuevos, double peso, boolean enPeligroExtincion, String dieta) {
-        super(nombreCientifico, descripcionHabitat, esperanzaVida);
-        this.temperaturaCorporal = temperaturaCorporal;
-        this.cantidadHuevos = cantidadHuevos;
-        this.peso = peso;
-        this.enPeligroExtincion = enPeligroExtincion;
-        this.dieta = dieta;
-    }
+	// Constructor
+	public Reptil(String nombreCientifico, String descripcionHabitat, int esperanzaVida, double temperaturaCorporal,
+			int cantidadHuevos, double peso, boolean enPeligroExtincion, String dieta) {
+		super(nombreCientifico, descripcionHabitat, esperanzaVida);
+		this.temperaturaCorporal = temperaturaCorporal;
+		this.cantidadHuevos = cantidadHuevos;
+		this.peso = peso;
+		this.enPeligroExtincion = enPeligroExtincion;
+		this.dieta = dieta;
+	}
 
-    //Setters & getters
-    public double getTemperaturaCorporal() {
+	// Setters & getters
+	public double getTemperaturaCorporal() {
 		return this.temperaturaCorporal;
 	}
 
@@ -56,16 +57,33 @@ public class Reptil extends Animal {
 		this.dieta = dieta;
 	}
 
-    //Methods 
-    public double comidaDiaria() {
-        return 0;
-    }
+	// Methods
+	public double comidaDiaria() {
+		// Dependerá si es serpiente o Reptil Acuático
+		if (this instanceof Serpiente) {
+			Serpiente serpiente = (Serpiente) this; // Transformo el objeto actual a Serpiente
+			if (serpiente.getLongitud() < 1) { // Si la longitud es menor a 1
+				return serpiente.getPeso() * (0.02 + Math.random() * (0.05 - 0.02)) // Número random entre 0.02 y 0.05 
+			} else if (serpiente.getLongitud() >= 1 && serpiente.getLongitud() <= 2) { // si la longitud está entre 1 y 2 
+				return serpiente.getPeso() * (0.03 + Math.random() * (0.06 - 0.03)) // Consume entre 3 a 6% de su peso
+			} else {
+				return serpiente.getPeso() * (0.04 + Math.random() * (0.08 - 0.04)) // Consume entre 4 a 8% de su peso
+			}
+		} else if (this instanceof ReptilAcuatico) {
+			ReptilAcuatico reptilacuatico = (ReptilAcuatico) this;
+			
+		}
+		return 0;
+	}
 
-    public double costoMantenimiento() {
-        return 0;
-    }
+	public double costoMantenimiento() {
+		return 0;
+	}
 
-    public String getInfo(){
-        return "Reptil: " + super.getNombreCientifico() + ", Esperanza de vida: " + super.getEsperanzaVida() + ", Temperatura corporal: " + this.temperaturaCorporal + ", Cantidad de huevos: " + this.cantidadHuevos + ", Peso: " + this.peso + ", En peligro de extinción: " + this.enPeligroExtincion + ", Dieta: " + this.dieta;
-    }
+	public String getInfo() {
+		return "Reptil: " + super.getNombreCientifico() + ", Esperanza de vida: " + super.getEsperanzaVida()
+				+ ", Temperatura corporal: " + this.temperaturaCorporal + ", Cantidad de huevos: " + this.cantidadHuevos
+				+ ", Peso: " + this.peso + ", En peligro de extinción: " + this.enPeligroExtincion + ", Dieta: "
+				+ this.dieta;
+	}
 }
--- ReptilAcuatico.java
+++ ReptilAcuatico.java
@@ -2,46 +2,60 @@ public class ReptilAcuatico extends Reptil {
     private String tipoAgua;
     private double velocidadNado;
     private int duracionBuceo;
+    private double longitud; // En centímetros (cm)
 
-    //Constructor
-    public ReptilAcuatico(String nombreCientifico, String descripcionHabitat, int esperanzaVida, double temperaturaCorporal,int cantidadHuevos, double peso, boolean enPeligroExtincion, String dieta, String tipoAgua,double velocidadNado, int duracionBuceo) {
-        super(nombreCientifico, descripcionHabitat, esperanzaVida, temperaturaCorporal, cantidadHuevos, peso, enPeligroExtincion, dieta);
+    // Constructor
+    public ReptilAcuatico(String nombreCientifico, String descripcionHabitat, int esperanzaVida,
+            double temperaturaCorporal, int cantidadHuevos, double peso, boolean enPeligroExtincion, String dieta,
+            double longitud, String tipoAgua, double velocidadNado, int duracionBuceo) {
+        super(nombreCientifico, descripcionHabitat, esperanzaVida, temperaturaCorporal, cantidadHuevos, peso,
+                enPeligroExtincion, dieta);
         this.tipoAgua = tipoAgua;
+        this.longitud = longitud;
         this.velocidadNado = velocidadNado;
         this.duracionBuceo = duracionBuceo;
     }
 
-    //Setters & Getters
+    // Setters & Getters
     public String getTipoAgua() {
-		return this.tipoAgua;
-	}
+        return this.tipoAgua;
+    }
 
-	public void setTipoAgua(String tipoAgua) {
-		this.tipoAgua = tipoAgua;
-	}
+    public void setTipoAgua(String tipoAgua) {
+        this.tipoAgua = tipoAgua;
+    }
 
-	public double getVelocidadNado() {
-		return this.velocidadNado;
-	}
+    public double getVelocidadNado() {
+        return this.velocidadNado;
+    }
 
-	public void setVelocidadNado(double velocidadNado) {
-		this.velocidadNado = velocidadNado;
-	}
+    public void setVelocidadNado(double velocidadNado) {
+        this.velocidadNado = velocidadNado;
+    }
 
-	public int getDuracionBuceo() {
-		return this.duracionBuceo;
-	}
+    public int getDuracionBuceo() {
+        return this.duracionBuceo;
+    }
 
-	public void setDuracionBuceo(int duracionBuceo) {
-		this.duracionBuceo = duracionBuceo;
-	}
+    public void setDuracionBuceo(int duracionBuceo) {
+        this.duracionBuceo = duracionBuceo;
+    }
 
-    //Methods
+    public double getLongitud() {
+        return this.longitud;
+    }
+
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
+
+    // Methods
     public double tamañoRecinto() {
         return 0;
     }
 
-    public String getInfo(){
-        return "Reptil Acuatico: " + super.getInfo() + ", Tipo Agua: " + this.tipoAgua + ", Velocidad Nado: " + this.velocidadNado + ", Duracion Buceo: " + this.duracionBuceo;
+    public String getInfo() {
+        return "Reptil Acuatico: " + super.getInfo() + ", Tipo Agua: " + this.tipoAgua + ", Velocidad Nado: "
+                + this.velocidadNado + ", Duracion Buceo: " + this.duracionBuceo + ", Longitud (cm): " + this.longitud;
     }
 }
